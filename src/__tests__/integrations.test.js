acb3cf716e53f37f7e948fad6c4c7d9ec9214fa1
ericscottmarquez <ericscottmarquez@gmail.com>
2018-12-26 01:47:23 -0700

    first commit

---
+++ src/__tests__/integrations.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import moxios from 'moxios';
+import Root from 'Root';
+import App from 'components/App';
+
+
+beforeEach(() => {
+  moxios.install();
+  moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+    status: 200,
+    response: [{ name: 'Fetched #1'}, { name: 'Fetched #2'} ]
+  });
+});
+
+afterEach(() => {
+  moxios.uninstall();
+
+});
+
+it('can fetch a list of comments and display them', () => {
+  //try to render whole app
+  const wrapped = mount(
+    <Root>
+      <App/>
+    </Root>
+  );
+  //find the fetch comments button and click it:
+  wrapped.find('.fetch-comments').simulate('click');
+  //relax boi:
+  
+  moxios.wait(() => {
+    wrapped.update();
+    //expect to find a list of comments:
+    expect(wrapped.find('li').length).toEqual(2);
+
+    done();
+    wrapped.unmount();
+  });
+
+});
+
